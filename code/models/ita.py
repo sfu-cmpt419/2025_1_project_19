0ffef08a2a4f347bca321fa2da819d630eb24ac2
Aryaman Bahuguna <aba214@sfu.ca>
2025-03-22 15:47:28 +0530

    ITA conversions to monk and fitzpatrick
    added function to convert ITA scale to fitzpatrick and monk scale; results in  directory

--- code/models/ita.py
+++ code/models/ita.py
@@ -11,6 +11,32 @@ folder_label_map = {
 }
 out_path = '/Users/aryaman_bahuguna/Documents/Spring2025/CMPT419/project/2025_1_project_19/outputs/'
 
+# -- helper functions -- #
+
+def compute_ita(l_values, b_values):
+    # Compute ITA
+    epsilon = 1e-6 # avoid division by zero
+    ita = np.arctan((l_values - 50) / (b_values + epsilon)) * (180 / np.pi)
+    return ita
+
+def ita_to_fitzpatrick(ita):
+    if ita > 55:
+        return 'I'
+    elif ita > 41:
+        return 'II'
+    elif ita > 28:
+        return 'III'
+    elif ita > 10:
+        return 'IV'
+    elif ita > -30:
+        return 'V'
+    else:
+        return 'VI'
+
+def ita_to_monk_scale(ita, min_ita=-60, max_ita=90):
+    ita_clipped = np.clip(ita, min_ita, max_ita)
+    return int(np.round(1 + 9 * (max_ita - ita_clipped) / (max_ita - min_ita)))
+
 # Store results here
 results = []
 
@@ -42,24 +68,28 @@ for label, path in folder_label_map.items():
             l_mean = np.mean(l_vals)
             b_mean = np.mean(b_vals)
 
-            # Compute ITA
-            epsilon = 1e-6
-            ita = np.arctan((l_mean - 50) / (b_mean + epsilon)) * (180 / np.pi)
+            ita = compute_ita(l_mean, b_mean)
+
+            # conversions
+            fitzpatrick = ita_to_fitzpatrick(ita)
+            monk_scale = ita_to_monk_scale(ita)
 
             results.append({
                 'filename': filename,
                 'label': label,
                 'L*': l_mean,
                 'b*': b_mean,
-                'ITA': ita
+                'ITA': ita,
+                'fitzpatrick': fitzpatrick,
+                'monk': monk_scale
             })
 
         except Exception as e:
             print(f"Error processing {filename}: {e}")
 
 # Save results
-csv_path = os.path.join(out_path, 'ita-results-all.csv')
+csv_path = os.path.join(out_path, 'ita-fitz-monk.csv')
 df = pd.DataFrame(results)
 df.to_csv(csv_path, index=False)
 
-print("Successfull! ITA values saved to 'ita_results_all.csv'")
+print("Successfull! ITA values saved to 'ita-fitz-monk.csv'")
